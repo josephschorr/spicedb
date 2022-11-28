0a290aa26325e319f0768aec1ab5a5b14394976c
Joseph Schorr <josephschorr@users.noreply.github.com>
2022-11-28 15:21:46 -0500

    Add Index Only select test to PG datastore

--- internal/datastore/postgres/postgres.go
+++ internal/datastore/postgres/postgres.go
@@ -222,6 +222,7 @@ func newPostgresDatastore(
 		cancelGc:                cancelGc,
 		readTxOptions:           pgx.TxOptions{IsoLevel: pgx.RepeatableRead, AccessMode: pgx.ReadOnly},
 		maxRetries:              config.maxRetries,
+		queryLogger:             config.queryLogger,
 	}
 
 	datastore.SetOptimizedRevisionFunc(datastore.optimizedRevisionFunc)
@@ -285,6 +286,8 @@ type pgDatastore struct {
 	gcGroup  *errgroup.Group
 	gcCtx    context.Context
 	cancelGc context.CancelFunc
+
+	queryLogger datastore.QueryLoggerForTesting
 }
 
 func (pgd *pgDatastore) SnapshotReader(revRaw datastore.Revision) datastore.Reader {
@@ -306,7 +309,7 @@ func (pgd *pgDatastore) SnapshotReader(revRaw datastore.Revision) datastore.Read
 	}
 
 	querySplitter := common.TupleQuerySplitter{
-		Executor:         pgxcommon.NewPGXExecutor(createTxFunc),
+		Executor:         pgxcommon.NewPGXExecutor(createTxFunc, pgd.queryLogger),
 		UsersetBatchSize: pgd.usersetBatchSize,
 	}
 
@@ -340,7 +343,7 @@ func (pgd *pgDatastore) ReadWriteTx(
 			}
 
 			querySplitter := common.TupleQuerySplitter{
-				Executor:         pgxcommon.NewPGXExecutor(longLivedTx),
+				Executor:         pgxcommon.NewPGXExecutor(longLivedTx, pgd.queryLogger),
 				UsersetBatchSize: pgd.usersetBatchSize,
 			}
 
